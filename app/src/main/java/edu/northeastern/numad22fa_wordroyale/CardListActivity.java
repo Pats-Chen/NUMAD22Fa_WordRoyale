6155aff689b63a37124b9cd0858d3083adf73eb0
Jiazhe Chen <chenjiazhe@outlook.com>
2022-12-08 16:45:31 -0500

    Implement card info list from firebase function

--- app/src/main/java/edu/northeastern/numad22fa_wordroyale/CardListActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_wordroyale/CardListActivity.java
@@ -25,8 +25,11 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,13 +53,29 @@ public class CardListActivity extends AppCompatActivity {
             return;
         }
         setContentView(R.layout.activity_card_list);
-        cardListRV = findViewById(R.id.cardListRV);
 
         cardList = new ArrayList<>();
-        cardList.add(new Card("0001", "测试", "ceshi"));
-        cardList.add(new Card("0002", "单词", "danci"));
-
         cardListAdapter = new CardAdapter(this, cardList);
+        rootRef.child("users")
+                .child(userAuth.getCurrentUser().getUid())
+                .child("cardList").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Card card = dataSnapshot.getValue(Card.class);
+                            cardList.add(card);
+                        }
+                        cardListAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        cardListRV = findViewById(R.id.cardListRV);
+        cardListRV.setHasFixedSize(true);
         cardListRV.setAdapter(cardListAdapter);
         cardListRV.setLayoutManager(new LinearLayoutManager(this));
     }
